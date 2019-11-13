b72d5a7039562e4b9a31ac8b19a02ae05da79f54
Laura-Santos <56320746+Laura-Santos@users.noreply.github.com>
2019-11-13 15:03:04 -0600

    Agregando los atributos para empezar a trabajar en la MainActivity

---
+++ app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.proyectofinalsis22a;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et_codigo, et_descripcion, et_autor, et_tipo;
+    private Button btn_guardar, btn_consultaCodigo, btn_consultaDescripcion, btn_eliminar, btn_actualizar;
+
+
+
+}
