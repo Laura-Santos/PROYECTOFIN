8279243c7ed0e0e05b58abca9030af8d7e882026
Laura-Santos <56320746+Laura-Santos@users.noreply.github.com>
2019-11-13 15:42:23 -0600

    Agregué la clase Dto

---
+++ app/src/main/java/com/example/proyectofinalsis22a/Dto.java
@@ -0,0 +1,9 @@
+package com.example.proyectofinalsis22a;
+
+
+import java.io.Serializable;
+
+//Para enviar objetos entre actividades (Como parámetro) se coloca el "implements Serializable"
+public class Dto implements Serializable {
+
+}
--- app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
@@ -40,4 +40,5 @@ public class MainActivity extends AppCompatActivity {
     MantenimientoMySQL manto = new MantenimientoMySQL();
 
 
+
 }
