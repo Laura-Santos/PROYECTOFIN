032e784d445ec3e03275966f749fa01ed3acee81
Laura-Santos <56320746+Laura-Santos@users.noreply.github.com>
2019-11-14 10:06:26 -0600

    Agregando una alerta que pregunta si desea salir de la app

--- app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
@@ -47,5 +47,26 @@ public class MainActivity extends AppCompatActivity {
     //Alerta
     AlertDialog.Builder dialogo;
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finishAffinity();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
 
 }
