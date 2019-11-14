88903a80f1234e2dde6fc45406065ea624421826
alvarenga2019 <adaluzalvarengabelloso@gmail.com>
2019-11-14 17:52:07 -0600

    Agregue mas codigo en la clase MantenimientoMySQL

--- app/src/main/java/com/example/proyectofinalsis22a/MantenimientoMySQL.java
+++ app/src/main/java/com/example/proyectofinalsis22a/MantenimientoMySQL.java
@@ -100,3 +100,26 @@ Dto datos = new Dto();
                     @Override
                     public void onResponse(String response) {
 
+                        try {
+                            JSONObject requestJSON = new JSONObject(response.toString());
+                            String estado = requestJSON.getString("estado");
+                            String mensaje = requestJSON.getString("mensaje");
+
+                            if (estado.equals("1")) {
+                                //Toast.makeText(context, mensaje, Toast.LENGTH_SHORT).show();
+                                estadoGuardar = true;
+                            } else if (mensaje.equals("2")) {
+                                Toast.makeText(context, "Error. No se pudo guardar.\n" +
+                                        "Intentelo mas tarde.", Toast.LENGTH_SHORT).show();
+                                estadoGuardar = false;
+                            }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        //Toast.makeText(context, "Se encontrarón problemas...", Toast.LENGTH_SHORT).show();
+                        estadoGuardar = false;
+                    }
+
+                }
+
+
