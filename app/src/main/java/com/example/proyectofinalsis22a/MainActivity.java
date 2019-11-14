bc41182618221de66e7928da09aa16bdba40fae7
blanca12344 <rosalopezmancia1234@gmail.com>
2019-11-14 12:45:27 -0600

    agrege el primer bloque del boton actualizar en la MainActivity

--- app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalsis22a/MainActivity.java
@@ -229,3 +229,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(et_codigo.getText().toString().length()==0){
+                    et_codigo.setError("campo obligatorio");
+                    inputEt = false;
+                }else {
+                    inputEt=true;
+                }
+
+
