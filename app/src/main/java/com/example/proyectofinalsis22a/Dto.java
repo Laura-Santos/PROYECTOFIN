22314e7191bfdd529f6c32faa2024fa54133bb99
alvarenga2019 <adaluzalvarengabelloso@gmail.com>
2019-11-14 11:07:42 -0600

    Agregue las variables en la clase Dto

--- app/src/main/java/com/example/proyectofinalsis22a/Dto.java
+++ app/src/main/java/com/example/proyectofinalsis22a/Dto.java
@@ -6,4 +6,8 @@ import java.io.Serializable;
 //Para enviar objetos entre actividades (Como parámetro) se coloca el "implements Serializable"
 public class Dto implements Serializable {
 
+    int codigo;
+    String descripcion;
+    String autor;
+    String tipo;
 }
