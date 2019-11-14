7dbe0d7a63f0cb48adc761d3ba721d91d12e2b6e
alvarenga2019 <adaluzalvarengabelloso@gmail.com>
2019-11-14 13:49:56 -0600

    Agregue un nuevo Overrible

--- app/src/main/java/com/example/proyectofinalsis22a/MantenimientoMySQL.java
+++ app/src/main/java/com/example/proyectofinalsis22a/MantenimientoMySQL.java
@@ -67,7 +67,15 @@ Dto datos = new Dto();
 
                         }catch (JSONException e){
                             e.printStackTrace();
-                            //Toast.makeText(context, "Se encontrarón problemas...", Toast.LENGTH_SHORT).show();
                         }
 
                     }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //En este método se notifica al usuario acerca de un posible error al tratar de
+                //realizar una acción cualquier en la base de datos remota.
+                Toast.makeText(context, "No se puedo guardar. \n" +
+                        "Verifique su acceso a internet.", Toast.LENGTH_SHORT).show();
+            }
+        })
