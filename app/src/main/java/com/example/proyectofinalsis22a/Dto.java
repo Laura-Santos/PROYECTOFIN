d1d2ce3f17a303508f31fb09d2abfb7451d96b7a
alvarenga2019 <adaluzalvarengabelloso@gmail.com>
2019-11-14 11:12:38 -0600

    Le agregue los get y set de cada una de las variables creadas en la clase Dto

--- app/src/main/java/com/example/proyectofinalsis22a/Dto.java
+++ app/src/main/java/com/example/proyectofinalsis22a/Dto.java
@@ -10,4 +10,37 @@ public class Dto implements Serializable {
     String descripcion;
     String autor;
     String tipo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 }
