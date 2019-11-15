f784e1ac607ba60a4bfefc99f96f907181c9f6d3
blanca12344 <rosalopezmancia1234@gmail.com>
2019-11-15 09:17:46 -0600

    Ala java class Consulta_RecyclerView  se le agrego codigo

--- app/src/main/java/com/example/proyectofinalsis22a/Consulta_RecyclerView.java
+++ app/src/main/java/com/example/proyectofinalsis22a/Consulta_RecyclerView.java
@@ -1,4 +1,18 @@
 package com.example.proyectofinalsis22a;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 public class Consulta_RecyclerView {
+    //private static final String URL = "http://mjgl.com.sv/mysqlcrud/Api.php";
+    private static final String URL = Config.urlConsultaApiMySQLi;
+
+    List<Productos> productosList;
+    RecyclerView recyclerView;
+
+    ProductsAdapter adapter;
+
+    AlertDialog.Builder dialogo;
 }
