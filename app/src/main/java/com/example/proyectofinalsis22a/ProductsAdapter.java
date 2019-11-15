ad5ac4ad2eee668c3b141efd9a911f59b8376632
blanca12344 <rosalopezmancia1234@gmail.com>
2019-11-15 08:49:59 -0600

    cree la java class ProductsAdapter con lo ultimo de codigo

--- app/src/main/java/com/example/proyectofinalsis22a/ProductsAdapter.java
+++ app/src/main/java/com/example/proyectofinalsis22a/ProductsAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -53,3 +55,25 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
         holder.textViewAutor1.setText(product.getAutor());
         holder.textViewTipo1.setText(product.getTipo());
     }
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewCodigo1, textViewDescripcion1, textViewAutor1, textViewTipo1;
+        ImageView imageView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            textViewCodigo1 = itemView.findViewById(R.id.textViewCodigo1);
+            textViewDescripcion1 = itemView.findViewById(R.id.textViewDescripcion1);
+            textViewAutor1= itemView.findViewById(R.id.textViewAutor1);
+            textViewTipo1 = itemView.findViewById(R.id.textViewTipo1);
+        }
+    }
+
+
+}
