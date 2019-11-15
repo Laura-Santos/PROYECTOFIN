68ebf6c8b7876f3eec88c870d9de7f048c3e7ff2
Laura-Santos <garcialorenasantos@gmail.com>
2019-11-14 23:50:52 -0600

    Agregando clase MySingleton

---
+++ app/src/main/java/com/example/proyectofinalsis22a/MySingleton.java
@@ -0,0 +1,20 @@
+package com.example.proyectofinalsis22a;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import androidx.collection.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+//import android.util.LruCache;
+//import android.support.v4.util.LruCache;
+//import android.support.v4.util.LruCache;
+
+public class MySingleton {
+
+}
+
