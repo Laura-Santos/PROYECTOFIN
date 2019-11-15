6df1028947fc1c97ac9a86f23d918e8268274437
blanca12344 <rosalopezmancia1234@gmail.com>
2019-11-15 08:48:51 -0600

    cree la java class ProductsAdapter con los parametros

--- app/src/main/java/com/example/proyectofinalsis22a/ProductsAdapter.java
+++ app/src/main/java/com/example/proyectofinalsis22a/ProductsAdapter.java
@@ -1,6 +1,9 @@
 package com.example.proyectofinalsis22a;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,4 +18,38 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
     public ProductsAdapter(Context mCtx, List<Productos> productList) {
         this.mCtx = mCtx;
         this.productList = productList;
+    }
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.list_layout, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Productos product = productList.get(position);
+
+        //loading the image
+
+        //String im = product.getImagen();
+        //Toast.makeText(mCtx, ""+im, Toast.LENGTH_SHORT).show();
+        //String im = product.getCodigo();
+
+        // if(im.isEmpty()) {
+        holder.imageView.setImageResource(R.drawable.imgnoencontrada);
+        holder.textViewCodigo1.setText(String.valueOf(product.getCodigo()));
+        holder.textViewDescripcion1.setText(product.getDescripcion());
+        holder.textViewAutor1.setText(product.getAutor());
+        holder.textViewTipo1.setText(product.getTipo());
+
+        //   }else{
+        // Glide.with(mCtx)
+        //       .load(product.getImagen())
+        //     .into(holder.imageView);
+
+        holder.textViewCodigo1.setText(String.valueOf(product.getCodigo()));
+        holder.textViewDescripcion1.setText(product.getDescripcion());
+        holder.textViewAutor1.setText(product.getAutor());
+        holder.textViewTipo1.setText(product.getTipo());
     }
