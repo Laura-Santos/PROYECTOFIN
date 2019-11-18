4755b8c6eb514ae41c07ed69d018106c26c3b2ee
blanca12344 <rosalopezmancia@gmail.com>
2019-11-18 10:53:34 -0600

    se areglaron algunos errores que aparecieron por agregar mas en otra clase

--- app/src/main/java/com/example/proyectofinalsis22a/Inicio.java
+++ app/src/main/java/com/example/proyectofinalsis22a/Inicio.java
@@ -1,12 +1,14 @@
 package com.example.proyectofinalsis22a;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-
 public class Inicio extends AppCompatActivity {
+    private Button btn_consultaNumero, btn_consultaporNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,24 @@ public class Inicio extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-            }
+        btn_consultaNumero = findViewById(R.id.btn_consultaNumero);
+        btn_consultaporNombre = findViewById(R.id.btn_consultaporNombre);
+
+    }
+
+    public void numerohimno(View view){
+        Intent i  = new Intent(this, mostrarNumero.class);
+        startActivity(i);
+
+
+
+
+    }
+
+    public void nombreAutor(View view){
+        Intent i = new Intent(this, NombreAutor.class );
+        startActivity(i);
+    }
 
 }
+
